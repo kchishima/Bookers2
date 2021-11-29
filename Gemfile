51aa607e3c067cf23b96d613a601ac562781d75f
EC2 Default User <ec2-user@ip-172-31-27-151.ec2.internal>
2021-11-29 00:24:29 +0000

    [Finish]

--- Gemfile
+++ Gemfile
@@ -73,3 +73,4 @@ gem 'font-awesome-sass', '~> 5.13'
 
 gem "refile", require: "refile/rails", github: 'manfe/refile'
 gem "refile-mini_magick"
+
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'e45d3e09307032b8c0e2f9abbf1ce9e9ecec642aa04dbee9f7c9051aa513410a91b308cb1e5ace16ffafa79f777eaa7d23ade99a60bcca6fa1974ee0ad0527b5'
